dccee6d987ff231334505c21bd38155c48138c84
uvlad7 <uvlad7@gmail.com>
2021-07-18 04:51:55 +0300

    Finished

--- Lecture10-Classes/attr_accessor.rb
+++ Lecture10-Classes/attr_accessor.rb
@@ -2,7 +2,8 @@ class Person
   attr_accessor :name, :age # getters and setters for name and age
 end 
 
-person1 = Person.new 
+person1 = Person.new
+p person1.instance_variables
 p person1.name # => nil 
 person1.name = "Mike" 
 person1.age = 15 
@@ -10,3 +11,4 @@ puts person1.name # => Mike
 puts person1.age # => 15 
 person1.age = "fifteen" 
 puts person1.age # => fifteen 
+p person1.instance_variables
